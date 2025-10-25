46f0d0bfad1c595e98da0f4393e627a5cdd77a49
Sushanth Vaddi <sushanthweb@gmail.com>
2025-10-25 14:12:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,6 @@ app.use('/api/todos',todoRoutes)
 //connect to svrer
 mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://sushanthvaddi123_db_user:4gGhW1JOcN3L1h2D@cluster0.8heh50z.mongodb.net/')
   .then(() => {
-    app.listen(process.env.PORT || 5000, () =>{
-      console.log(`server is running on port ${process.env.PORT || 5000}`)})
+    app.listen( 5000, () =>{
+      console.log(`server is running on port 5000`)})
   }).catch((error)=>console.log('error:',error))
